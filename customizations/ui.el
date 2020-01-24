a784c05376db3c7d1af313ccd19b6ba3869a4513
Lucas Peres <lucasperes95@gmail.com>
2020-01-24 05:58:11 -0300

    Customizing dashboard

--- customizations/ui.el
+++ customizations/ui.el
@@ -1,14 +1,22 @@
-;; Changes startup screen.
-(require 'dashboard)
-(dashboard-setup-startup-hook)
 
-;; Add custom icons.
 (require 'all-the-icons)
 
 ;; Displays ugly form feed characters as
-;; tidy horzontal rules.
+;; tidy horizontal rules.
 ;; Dependency for dashboard package.
 (require 'page-break-lines)
+
+;; Changes startup screen.
+(require 'dashboard)
+(dashboard-setup-startup-hook)
+(setq dashboard-startup-banner 'logo)     ; displays an alternative Emacs logo
+(setq dashboard-center-content t)         ; center the content
+(setq dashboard-show-shortcuts nil)       ; disable shortcut "jump" indicators for each section
+(setq dashboard-set-heading-icons t)      ; add icons to headings
+(setq dashboard-set-file-icons t)         ; add icons to files 
+(setq dashboard-items '((recents . 10)
+			(projects . 5)
+			(bookmarks . 5))) ; customize which widgets are displayed
 
 ;; Themes
 (load-theme 'spacemacs-light t)
