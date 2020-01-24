f81d1f3a3f348e64c47bc22a8f26f9621aa60f77
Lucas Peres <lucasperes95@gmail.com>
2020-01-24 04:22:27 -0300

    Enabling all-the-icons package

--- customizations/ui.el
+++ customizations/ui.el
@@ -2,5 +2,13 @@
 (require 'dashboard)
 (dashboard-setup-startup-hook)
 
+;; Add custom icons.
+(require 'all-the-icons)
+
+;; Displays ugly form feed characters as
+;; tidy horzontal rules.
+;; Dependency for dashboard package.
+;(require 'page-break-lines)
+
 ;; Themes
 (load-theme 'spacemacs-light t)
