de393c9f525357cc6c161a517a3315637df4b9c9
Lucas Peres <lucasperes95@gmail.com>
2020-01-25 04:41:37 -0300

    Adding comment func and tabs to 4

--- customizations/editing.el
+++ customizations/editing.el
@@ -1,9 +1,24 @@
-;; Globally prettify symbols
+;; Globally prettify symbols.
 (global-prettify-symbols-mode 1)
 (setq prettify-symbols-alist '(("lambda" . 955)))
 
-;; Don't use hard tabs
+;; Use spaces for tab.
 (setq-default indent-tabs-mode nil)
+
+;; Comments.
+(defun toggle-comment-on-line ()
+  "Comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(global-set-key (kbd "C-;") 'toggle-comment-on-line)
+
+;; Use 4 spaces for tabs.
+(defun die-tabs ()
+  (interactive)
+  (set-variable 'tab-width 4)
+  (mark-whole-buffer)
+  (untabify (region-beginning) (region-end))
+  (keyboard-quit))
 
 ;; Key binding to use "hippie expand" for text autocompletion.
 ;; http://www.emacswiki.org/emacs/HippieExpand
