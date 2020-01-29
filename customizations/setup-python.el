0eabf54ba694aaf1877d4f8521d3862afb71b583
Lucas Peres <lucasperes95@gmail.com>
2020-01-29 19:38:01 -0300

    Enabling flycheck and company-jedi

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -1,6 +1,16 @@
 ;; Enable Elpy by default
 (elpy-enable)
 
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
 ;; Standard Jedi.el setting
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot 1)
+;(add-hook 'python-mode-hook 'jedi:setup)
+;(setq jedi:complete-on-dot 1)
+
+;; Standard company-jedi setting
+(defun my/python-mode-hook ()
+  add-to-list 'company-backends 'company-jedi)
+(add-hook 'python-mode-hook 'my/python-mode-hook)
